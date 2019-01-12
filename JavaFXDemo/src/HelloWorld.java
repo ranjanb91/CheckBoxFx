8a43b2b1cd66592945e1aa25398035dacdfd5f50
Rashmi Ranjan Behera <46593467+ranjanb91@users.noreply.github.com>
2019-01-12 09:46:56 +0530

    Add files via upload

---
+++ JavaFXDemo/src/HelloWorld.java
@@ -0,0 +1,34 @@
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application {
+
+	public static void main(String[] args) {
+		
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("My Title");
+		primaryStage.setScene(scene);
+		
+		primaryStage.show();
+		
+	}
+
+}
